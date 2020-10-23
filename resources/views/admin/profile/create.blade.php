848bcdf1d6b297417d4497cd8a01afde6edd3e44
riemurata <riemura0303@yahoo.co.jp>
2020-10-23 09:45:08 +0000

    kadai3

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>PHP/Laravel 10</title>
+    </head>
+    <body>
+        <h1>課題</h1>
+        <h2>public function add()<br>
+         return view('admin.profile.create');</h2>
+        <p>このadd actionは、resources/views/admin/profile/ディレクトリの中に、create.blade.phpを設置。</p>
+    </body>
+    
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>PHP/Laravel 10</title>
+    </head>
+    <body>
+        <h1>課題</h1>
+         <h2>public function edit()<br>
+          return view('admin.profile.edit');</h2>
+        <p>このedit actionは、resources/views/admin/profileディレクトリの中に、edit.blade.phpを設置。</p>
+    </body>
+    
+</html>
