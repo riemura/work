148f00ed0ac1c7117d3b84941c16413f7bc38bb9
riemurata <riemura0303@yahoo.co.jp>
2020-11-03 09:21:06 +0000

    kadai14

--- database/migrations/2020_11_03_084005_create_profile_table.php
+++ database/migrations/2020_11_03_084005_create_profile_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNewsTable extends Migration
+class CreateProfileTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateNewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('news', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('title');
-            $table->string('body');
-            $table->string('image_path')->nullable();
+        Schema::create('profile', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('gender');
+            $table->string('hobby');
+            $table->string('introduction');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateNewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('news');
+        Schema::dropIfExists('profile');
     }
 }
