2e9d318ea4fcc41efaee734ac733443a688d5afd
riemurata <riemura0303@yahoo.co.jp>
2020-11-26 10:05:38 +0000

    kadai16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -26,14 +26,18 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+    return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin.profile.edit');
+        return redirect('admin/profile/edit');
     }
     
 }
